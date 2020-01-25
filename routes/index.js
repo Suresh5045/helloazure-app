7cb5c6a33a17242b7812114a5afc76bcd98b1faa
Suresh Kumar Vattam <sureshv@ispace.com>
2020-01-25 12:22:21 +0530

    slot-deployment

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var model = {
+title: 'Azure',
+message : process.env.message || "This is development"
+  };
   res.render('index', { title: 'Azure' });
 });
 
